ee2f3e52933ef17042a6a415f121b694d5569fff
OwenBurroughs <9540178+owenburroughs@users.noreply.github.com>
2026-09-03 10:16:43 -0500

    Actually moved out old files so Bacilliscope conforms to best practice for snakemake workflow

--- HTS_Bacilliscope/DatabaseTools/SimpleSQLQueryHander.py
+++
@@ -1,5 +0,0 @@
-#This is a simple function to execute SQL queries from a DuckDB connection or cursor. This is unneccesary now, but it will be useful to have this modularity in the future.
-def execute_SQL_query(con, query, dataframe=None):
-    #most of my queries use a dataframe as input
-    dataframe = dataframe
-    return con.execute(query)
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-BACILLISCOPE
+# BACILLISCOPE
+
+## To-do
+* Potentially add support for non-standard control wells for certain plates. No need to add this until it is required.
